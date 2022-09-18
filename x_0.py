9b71ade9978fd30ddbf5ea5c1607e4af9249d9ae
Hello-Dminty <moskvitin-d@mail.ru>
2022-09-18 15:15:14 +0300

    V2

--- x_0.py
+++ x_0.py
@@ -1,6 +1,7 @@
 #Крестики-нолики
 board = [['-'] * 3 for _ in range(3)]
 count = 0
+all_games = []
 #board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 def paint_board():
     #рисует игровое поле
@@ -17,12 +18,12 @@ def user(count=0):
             gamer = list(map(int, gamer))
             if 0 <= gamer[0] <= 2 and 0 <= gamer[1] <= 2 and board[gamer[0]][gamer[1]] == "-" \
             and len(gamer) == 2:
+                L,R = gamer
                 if count % 2:
-                    board[gamer[0]][gamer[1]] = "0"
+                    board[L][R] = "0"
                 else:
-                    board[gamer[0]][gamer[1]] = "X"
+                    board[L][R] = "X"
                 count += 1
-                
                 return count
             print("Неверный ввод. Ввести цифру вертик. и горизонт. через пробел ")    
             continue
@@ -38,36 +39,39 @@ def win(begin):
         x_o = "X"
     for i in range(3):
         for i in range(3):
-            combo = [[board[i][0],board[i][1], board[i][2]], [board[0][i], board[1][i], \
+            combo = [[board[i][0], board[i][1], board[i][2]], [board[0][i], board[1][i], \
                     board[2][i]], [board[0][0], board[1][1], board[2][2]], \
                      [board[2][0], board[1][1], board[0][2]]]
             for _ in combo:
                 if len([i for i, n in enumerate(_) if n == x_o ]) == 3:
                     return True
-def nex_game():
-    #продолжение
-    if input("Хотите сыграть еще? Введите Y или N ") == "Y":
-        board = [['-'] * 3 for _ in range(3)]
-        count = 0
-        user()
-    
+
 while True:
+    #цикл игры
     if count == 9:
         print("Ничья!")
         paint_board()
-        nex_game()
+        if input("Хотите сыграть еще? Введите Y ") == "Y":
+            board = [['-'] * 3 for _ in range(3)]
+            print (board)
+            count = 0
+            user()
     user(count)
-    if count == 9:
-        print()
     if win(count):
         if count % 2:
-            x_o = "0"
+            winer = "0"
         else:
-            x_o = "X"
+            winer = "X"
+        all_games += winer
         paint_board()
-        print(f"Победа игрока {x_o} !")
-        nex_game()
-        break
+        print(f"Победа игрока {winer}. Победы Х: {all_games.count('X')}, "
+        f"Победы 0: {all_games.count('0')}!")
+        if input("Хотите сыграть еще? Введите Y ") == "Y":
+            board = [['-'] * 3 for _ in range(3)]
+            count = 0
+            continue
+        else:
+            break
     count += 1
     continue
  
